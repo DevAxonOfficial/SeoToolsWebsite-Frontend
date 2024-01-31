df17327a8bdeea0a89a3a528f16863102acc20f7
Talha Usman <talhausman@devpumas.com>
2024-01-31 18:26:13 +0500

    fix headers

--- app/components/Header.js
+++ app/components/Header.js
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { ALL_TOOLS, ArrayOfUl } from "../utils/constants";
 import { Tools } from "../utils/constants";
 
+
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [icon, setIcon] = useState(true);
@@ -33,8 +34,8 @@ const Header = () => {
         </div>
         <div className="xm:hidden">
           <ul className="flex">
-            {Tools.map((Tool,index) => (
-              <li key={index}>
+            {Tools.map(Tool => (
+              <li>
               <Link className=" mx-4 font-semibold text-[#185058]" href={Tool.Url}>{Tool.Name}</Link>
             </li>
             ))}
@@ -42,27 +43,25 @@ const Header = () => {
         </div>
         </div>
         <div className="sm:hidden  h-16 md:hidden lg:flex xs:hidden items-center mt-1 md:mt-0 md:mr-6 md:ml-0 ml-10">
-          <div className="flex ml-4 md:ml-0 md:mr-2 mr-24 ">
-            <button className="bg-transparent hover:shodow-lg text-black hover:text-black md:py-2 md:px-4 hover:border-transparent rounded md:text-base text-sm">
-              Login
-            </button>
-          </div>
-          <div className="flex">
-            <button className="bg-red-500 hover:bg-red-300 text-white md:font-bold font-semibold md:py-2 md:px-4 p-2 text-xs md:text-base rounded-xl md:-mr-0 ">
-              Sign Up
-            </button>
-          </div>
+        <div className="flex ml-4 md:ml-0 md:mr-2 mr-24 ">
+          <button className="bg-transparent hover:shodow-lg text-black hover:text-black md:py-2 md:px-4 hover:border-transparent rounded md:text-base text-sm">
+            Login
+          </button>
+        </div>
+        <div className="flex">
+          <button className="bg-red-500 hover:bg-red-300 text-white md:font-bold font-semibold md:py-2 md:px-4 p-2 text-xs md:text-base rounded-xl md:-mr-0 ">
+            Sign Up
+          </button>
         </div>
       </div>
+      </div>
       {isOpen &&
-          <div className=" bg-[#F2F2F2] xm:pl-5 xm:block sm:flex justify-evenly" >
+          <div className="bg-[#F2F2F2] xm:pl-5 xm:block sm:flex justify-evenly" >
               <p className=" mb-4 text-sm font-semibold text-gray-500"> Convert And Compress  
             {ALL_TOOLS.convert_and_compress.map((Tool, index)=>{
-              
               return(
               <ul key={index}>
-              
-              <li className={` py-1 px-3 hover:cursor-pointer ${Tool.BgColor} hover:text-white rounded `}>
+              <li className={` py-1 px-3 hover:cursor-pointer hover:${Tool.BgColor} hover:text-white rounded `}>
                 <Link href={`${Tool.UrlLink}`}>
                 <div className="flex items-center ">
                 <Image src={Tool.Image} alt={Tool.Alt} width={30} height={30}   /> 
@@ -73,11 +72,11 @@ const Header = () => {
             </ul>)
             })}
             </p>
-            <div className=" mb-4 text-sm font-semibold text-gray-500"> Organize
+            <p className=" mb-4 text-sm font-semibold text-gray-500"> Organize
             {ALL_TOOLS.Organize.map((tool, index)=>{
               return(
               <ul  key={index}>
-              <li className={`  py-1 px-3 hover:cursor-pointer ${tool.BgColor} hover:text-white rounded `}>
+              <li className={`  py-1 px-3 hover:cursor-pointer hover:${tool.bgColor} hover:text-white rounded `}>
                 <Link href={`${tool.UrlLink}`}>
                 <div className="flex items-center ">
                 <Image src={tool.Image} alt={tool.Alt} width={30} height={30}   /> 
@@ -87,12 +86,12 @@ const Header = () => {
               </li>
             </ul>)
             })}
-            </div>
+            </p>
             <p className=" mb-4 text-sm font-semibold text-gray-500"> Convert to PDF
             {ALL_TOOLS.Convert_to_PDF.map((tool, index)=>{
               return(
               <ul  key={index}>
-              <li className={`  py-1 px-3 hover:cursor-pointer ${tool.BgColor} hover:text-white rounded `}>
+              <li className={`  py-1 px-3 hover:cursor-pointer hover:${tool.bgColor} hover:text-white rounded `}>
                 <Link href={`${tool.UrlLink}`}>
                 <div className="flex items-center ">
                 <Image src={tool.Image} alt={tool.Alt} width={30} height={30}   /> 
@@ -102,11 +101,11 @@ const Header = () => {
               </li>
             </ul>)
             })}
+            </p>
           </div>
-        </div>
-      )}
+          }
     </>
   );
-};
+};  
 
 export default Header;
