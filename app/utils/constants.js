7de46a78afaf0b1aa67be98e78a0d7dd90fcebd1
Talha Usman <talhausman@devpumas.com>
2024-02-07 20:22:21 +0500

    add link of reverse image

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -263,7 +263,7 @@ export const PROMPS_POPULAR_PDF_TOOLS = [
     img2: "/img/Group41.png",
     Link: "/pdf-to-word",
   },
-  {
+{
     bgcolor: "#f7e4f7",
     logoimage: "/img/SVG.png",
     heading: "SVG to PDF",
