f286044f5fc5687d3b4d38d70b7463b39f080c03
Talha Usman <talhausman@devpumas.com>
2024-02-26 16:26:56 +0500

    added ui and complete the remove pages tools

--- app/remove-pages/page.js
+++ app/remove-pages/page.js
@@ -1,13 +1,19 @@
 "use client";
+import dynamic  from "next/dynamic"
 import HandleDragnDrop from "../components/HandleDragnDrop";
+const FileConverter = dynamic(() => import("../components/FileConverter"), { ssr: false})
+// import FileConverter from "../components/FileConverter";
 import { REMOVE_TOOL_PROPS } from "../utils/constants";
 import {Adsterra,Adsterra1} from "../AdScript"
-const Page = () => (
-  <>
+import { useState } from "react";
+const Page = () => {
+const [file, setFile] = useState(null)
+  return<>
     <Adsterra slot={"32574d71c95c68d56e8efa676b6849a4"} />
-    <HandleDragnDrop {...REMOVE_TOOL_PROPS} />;
+    <HandleDragnDrop {...REMOVE_TOOL_PROPS} setUpperFile={setFile}  />
+    {file && <FileConverter pdfUrl={URL.createObjectURL(file)} /> }
     <Adsterra1 slot={"1a26ef3314cb0fb63793eb96ad8c870c"} />
   </>
-);
+};
 
 export default Page;
