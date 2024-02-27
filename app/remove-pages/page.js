0e08681f97efe3e1fc15bc10575ceb68f56adfe7
M.Owais Asad <152815891+Owi-Droid@users.noreply.github.com>
2024-02-27 19:30:45 +0500

    Added Remove Pages Content (#86)
    Co-authored-by: F3AROwi <owais.asad@devaxon.com>

--- app/remove-pages/page.js
+++ app/remove-pages/page.js
@@ -1,19 +1,25 @@
 "use client";
-import dynamic  from "next/dynamic"
+import dynamic from "next/dynamic";
 import HandleDragnDrop from "../components/HandleDragnDrop";
-const FileConverter = dynamic(() => import("../components/FileConverter"), { ssr: false})
+const FileConverter = dynamic(() => import("../components/FileConverter"), {
+  ssr: false,
+});
 // import FileConverter from "../components/FileConverter";
 import { REMOVE_TOOL_PROPS } from "../utils/constants";
-import {Adsterra,Adsterra1} from "../AdScript"
+import { Adsterra, Adsterra1 } from "../AdScript";
 import { useState } from "react";
+import Content from "../utils/content";
 const Page = () => {
-const [file, setFile] = useState(null)
-  return<>
-    <Adsterra slot={"32574d71c95c68d56e8efa676b6849a4"} />
-    <HandleDragnDrop {...REMOVE_TOOL_PROPS} setUpperFile={setFile}  />
-    {file && <FileConverter pdfUrl={URL.createObjectURL(file)} /> }
-    <Adsterra1 slot={"1a26ef3314cb0fb63793eb96ad8c870c"} />
-  </>
+  const [file, setFile] = useState(null);
+  return (
+    <>
+      <Adsterra slot={"32574d71c95c68d56e8efa676b6849a4"} />
+      <HandleDragnDrop {...REMOVE_TOOL_PROPS} setUpperFile={setFile} />
+      <Adsterra1 slot={"1a26ef3314cb0fb63793eb96ad8c870c"} />
+      {file && <FileConverter pdfUrl={URL.createObjectURL(file)} />}
+      <Content {...REMOVE_TOOL_PROPS} />
+    </>
+  );
 };
 
 export default Page;
